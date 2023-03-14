22cbd04c7f3af3894c4683380db2761c9e9ef94f
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-03-14 16:43:14 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+    return string.toUpperCase();
+    
+  }
+
+function whisper(string) {
+   return string.toLowerCase();
+   
+}
+
+// const spy = ("Hello");
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+
+
+
+function logWhisper(string){
+    console.log(string.toLowerCase())
+}
+function sayHiToHeadphonedRoommate(string) {
+    const cantHear = "I can't hear you!";
+    const yesIndeed = "YES INDEED!";
+    const loveTo = "I would love to!";
+    if (string.toLowerCase(string) === string) {
+      return cantHear;
+    }
+    else if (string.toUpperCase(string) === string) {
+      return yesIndeed;
+    }
+    else if ("Let's have dinner together!" === string) {
+      return loveTo
+    }
+  }
